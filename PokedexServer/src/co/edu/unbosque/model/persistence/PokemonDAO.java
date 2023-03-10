34a4f59f48e35271cd99edece4606f8301ae835e
Juan Diego Valderrama <114624562+sseuriracha@users.noreply.github.com>
2023-03-09 19:11:56 -0500

    Corrections made to both projects.

--- PokedexServer/src/co/edu/unbosque/model/persistence/PokemonDAO.java
+++ PokedexServer/src/co/edu/unbosque/model/persistence/PokemonDAO.java
@@ -1,6 +1,18 @@
 package co.edu.unbosque.model.persistence;
 
+import java.util.ArrayList;
+
+import co.edu.unbosque.model.PokemonDTO;
+
 public class PokemonDAO {
 	
+	private PokemonFile pokefile;
+	private ArrayList<PokemonDTO> pokelist;
+	
+	public PokemonDAO(PokemonFile pokefile) {
+		// TODO Auto-generated constructor stub
+		this.pokefile = pokefile;
+		pokelist = new ArrayList<PokemonDTO>();
+	}
 
 }
